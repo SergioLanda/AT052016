3021401baec95d71ba43f5b0a7e77e64244e8296
SergioLanda <sergio.landa@fundacion-jala.org>
2016-05-05 12:10:21 -0400

    Work01

---
+++ Sergio/print_practice_rb.rb
@@ -0,0 +1,20 @@
+#here begin print
+print "------"
+print "******"
+print "------"
+print "******"
+print "------"
+print "******"
+print "------"
+#here continues with puts
+puts  "******"
+puts  "------"
+puts  "******"
+puts  "------"
+puts  "******"
+puts  "------"
+puts  "******"
+=begin
+Practice - exercise for familiar with
+comments, print y put.	
+=end
